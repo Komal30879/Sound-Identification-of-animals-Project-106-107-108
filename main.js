f25ee30c6285d31e0a0e11ffa870b236baf7447f
Komal30879 <82501759+Komal30879@users.noreply.github.com>
2021-12-02 13:45:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,9 +18,12 @@ function gotResults(error,results){
         random_number_g = Math.floor(Math.random()*255) + 1;
         random_number_b = Math.floor(Math.random()*255) + 1;
 
+        console.log(random_number_r);
+
         document.getElementById("object").innerHTML = 'I can hear - ' + results[0].label;
-        document.getElementById("result_label").style.color = "rgb("+ random_number_r + "," + random_number_g + "," + random_number_b + ")";
-        document.getElementById("result_confidence").style.color = "rgb("+ random_number_r + "," + random_number_g + "," + random_number_b + ")";
+        document.getElementById("object").style.color = "rgb("+ random_number_r + "," + random_number_g + "," + random_number_b + ")";
+        document.getElementById("accuracy").style.color = "rgb("+ random_number_r + "," + random_number_g + "," + random_number_b + ")";
+        
 
         dog = 0;
         cat = 0;
@@ -35,23 +38,23 @@ function gotResults(error,results){
             dog = dog + 1;
             document.getElementById("accuracy").innerHTML = dog;
         } else if(results[0].label == "Meowing"){
-            img.src = 'cat.gif'
+            img.src = 'cat.gif';
             cat = cat + 1;
             document.getElementById("accuracy").innerHTML = cat;
         } else if(results[0].label == "Mooing"){
-            img.src = 'cow.gif'
+            img.src = 'cow.gif';
             cow = cow + 1;
             document.getElementById("accuracy").innerHTML = cow;
         } else if(results[0].label == "Chirping"){
-            img.src = 'bird.gif'
+            img.src = 'bird.gif';
             bird = bird + 1;
             document.getElementById("accuracy").innerHTML = bird;
         } else if(results[0].label == "Roaring"){
-            img.src = 'tiger.gif'
+            img.src = 'tiger.gif';
             tiger = tiger + 1;
             document.getElementById("accuracy").innerHTML = tiger;
         } else if(results[0].label == "Background Noise"){
-            img.src = 'ear.gif'
+            img.src = 'ear.gif';
             document.getElementById("accuracy").innerHTML = "";
         }
     }
